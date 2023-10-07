7356d854df3992441941344ab9b819641bb30fce
Thacku <140371109+thacku@users.noreply.github.com>
2023-10-07 23:05:30 +0530

    Update setup.js

--- src/bot/setup.js
+++ src/bot/setup.js
@@ -1,18 +1,18 @@
 const { Bot } = require("grammy");
-require("dotenv").config();
+const { BOT_TOKEN, OWNER_ID, START_MESSAGE } = require("../config/config");
 
 
-if (!process.env.BOT_TOKEN) {
+if (!process.env.BOT_TOKEN || !BOT_TOKEN) {
   console.error(`Invalid BOT_TOKEN`);
   return;
 }
 
-if (!process.env.OWNER_ID) {
+if (!process.env.OWNER_ID || !OWNER_ID) {
   console.error(`Invalid OWNER_ID`);
   return;
 }
 
-if (!process.env.START_MESSAGE) {
+if (!process.env.START_MESSAGE || !START_MESSAGE) {
   console.error(`Invalid START_MESSAGE`);
   return;
 }
@@ -22,13 +22,13 @@ const bot = new Bot(process.env.BOT_TOKEN);
 
 
 bot.catch((error) => {
-  console.log(`Error while handling update: ${error.ctx.update.update_id}`);
+  console.error(`Error while handling update: ${error.ctx.update.update_id}`);
   if (error.error instanceof GrammyError) {
-    console.log("Error in request:", error.error.description);
+    console.error("Error in request:", error.error.description);
   } else if (error.error instanceof HttpError) {
-    console.log("Could not contact Telegram:", error.error);
+    console.error("Could not contact Telegram:", error.error);
   } else {
-    console.log("Unknown error:", error.error);
+    console.error("Unknown error:", error.error);
   }
 });
 
@@ -36,9 +36,10 @@ bot.catch((error) => {
 bot.use(async (ctx, next) => {
   try {
     if (ctx.chat.type != "private") return;
+    await bot.api.sendChatAction(ctx.chat.id, "typing");
     await next();
   } catch (error) {
-    console.log("Error Occured", error.message);
+    console.error("Error Occured", error.message);
   }
 });
 
