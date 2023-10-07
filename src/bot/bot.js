74b96eac1aa5a58bdc5f941298d89e9e6e3facc4
Thacku <140371109+thacku@users.noreply.github.com>
2023-10-07 23:12:24 +0530

    Update bot.js

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,5 +1,8 @@
 const bot = require("./setup");
 const { OWNER_ID, START_MESSAGE } = require("../config/config");
+require("dotenv").config();
+
+
 const owner = process.env.OWNER_ID || OWNER_ID;
 const start_message = process.env.START_MESSAGE || START_MESSAGE;
 
@@ -8,7 +11,7 @@ bot.command("start", async (ctx) => {
   try {
     await bot.api.sendMessage(ctx.chat.id, start_message, { parse_mode: "HTML" });
   } catch (error) {
-    console.error("Error Occured", error.message);
+    console.log("Error Occured", error.message);
   }
 });
 
@@ -25,6 +28,6 @@ bot.on("message", async (ctx) => {
       await bot.api.forwardMessage(owner, ctx.chat.id, ctx.message.message_id);
     }
   } catch (error) {
-    console.error("Error Occured", error.message);
+    console.log("Error Occured", error.message);
   }
 });
